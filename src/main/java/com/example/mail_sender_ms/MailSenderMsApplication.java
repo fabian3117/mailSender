f0b5c769a2261507580bf1f83f64244479cc6fb2
fedeSportclub <federicogonzalez@sportclub.team>
2024-12-01 17:17:27 -0300

    First version

---
+++ src/main/java/com/example/mail_sender_ms/MailSenderMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mail_sender_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenderMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSenderMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mail_sender_ms/MailSenderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mail_sender_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
