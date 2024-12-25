abe8048195301a92bb32395e00679a5d7520d601
Fede <federicogonzalez@sportclub.team>
2024-12-25 14:10:14 -0300

    Change kafka lister

--- src/main/java/com/example/mail_sender_ms/kafka/KafkaMailConsumer.java
+++ src/main/java/com/example/mail_sender_ms/kafka/KafkaMailConsumer.java
@@ -2,15 +2,20 @@ package com.example.mail_sender_ms.kafka;
 
 import com.example.mail_sender_ms.dto.BodyMail;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,10 +33,58 @@ public class KafkaMailConsumer {
                 bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props,new StringDeserializer(), new CustomBodyMailDeserializer());
+    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, BodyMail> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, BodyMail> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    public class CustomBodyMailDeserializer implements Deserializer<BodyMail> {
+        @Override
+        public BodyMail deserialize(String topic, byte[] data) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                return objectMapper.readValue(data, BodyMail.class);
+            } catch (IOException e) {
+                throw new SerializationException("Error deserializing BodyMail", e);
+            }
+        }
     }
 
 
 
 }
+
+/*
+
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerSetting {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+
+    @Bean
+    public ConsumerFactory<String, BodyMail> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        /*props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS,false);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.example.mail_sender_ms.dto.BodyMail");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+//        return new DefaultKafkaConsumerFactory<>(props);
+
+@Bean
+public ConcurrentKafkaListenerContainerFactory<String, BodyMail> kafkaListenerContainerFactory() {
+ConcurrentKafkaListenerContainerFactory<String, BodyMail> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        */
